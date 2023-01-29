1cc5db5d93683bfa5353026f59ec9630b4d526ae
Justin Williams <williams.jman@gmail.com>
2023-01-29 17:16:00 -0500

    Created the getters and setters for users

--- src/main/java/edu/midlands/training/entities/Users.java
+++ src/main/java/edu/midlands/training/entities/Users.java
@@ -27,4 +27,74 @@ public class Users {
   @NotBlank(message = "type is a mandatory field")
   private String password;
 
+  public Users() {
+  }
+
+  public Users(String name, String title, String roles, String email, String password) {
+    this.name = name;
+    this.title = title;
+    this.roles = roles;
+    this.email = email;
+    this.password = password;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getRoles() {
+    return roles;
+  }
+
+  public void setRoles(String roles) {
+    this.roles = roles;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  @Override
+  public String toString() {
+    return "Users{" +
+        "id=" + id +
+        ", name='" + name + '\'' +
+        ", title='" + title + '\'' +
+        ", roles='" + roles + '\'' +
+        ", email='" + email + '\'' +
+        ", password='" + password + '\'' +
+        '}';
+  }
 }

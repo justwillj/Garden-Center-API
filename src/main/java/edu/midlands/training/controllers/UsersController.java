cd667af98860068593c19cf30634fef2ee56c051
Justin Williams <williams.jman@gmail.com>
2023-01-29 19:29:16 -0500

    Created the controller for the getUser by ID

--- src/main/java/edu/midlands/training/controllers/UsersController.java
+++ src/main/java/edu/midlands/training/controllers/UsersController.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,6 +33,13 @@ public class UsersController {
     logger.info(new Date() + LOGGER_REQUEST_RECEIVED + users.toString());
 
     return new ResponseEntity<>(usersService.queryUsers(users), HttpStatus.OK);
+  }
+
+  @GetMapping(value = "/{id}")
+  public ResponseEntity<Users> getUser(@PathVariable Long id) {
+    logger.info(new Date() + LOGGER_REQUEST_RECEIVED + id);
+
+    return new ResponseEntity<>(usersService.getUser(id), HttpStatus.OK);
   }
 
 }
--- src/main/java/edu/midlands/training/exceptions/ResourceNotFound.java
+++ src/main/java/edu/midlands/training/exceptions/ResourceNotFound.java
@@ -1,5 +1,9 @@
 package edu.midlands.training.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "The User isn't in the system")
 public class ResourceNotFound extends RuntimeException {
 
   public ResourceNotFound() {

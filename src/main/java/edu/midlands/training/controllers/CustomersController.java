c39a21755f7a90a5e1b45d3378471f2e58a4dd67
Justin Williams <williams.jman@gmail.com>
2023-01-31 23:45:31 -0500

    Added the delete mapping for customers

--- src/main/java/edu/midlands/training/controllers/CustomersController.java
+++ src/main/java/edu/midlands/training/controllers/CustomersController.java
@@ -2,6 +2,7 @@ package edu.midlands.training.controllers;
 
 import static edu.midlands.training.constants.StringConstants.CONTEXT_CUSTOMERS;
 import static edu.midlands.training.constants.StringConstants.CONTEXT_USERS;
+import static edu.midlands.training.constants.StringConstants.LOGGER_DELETE_REQUEST_RECEIVED;
 import static edu.midlands.training.constants.StringConstants.LOGGER_POST_REQUEST_RECEIVED;
 import static edu.midlands.training.constants.StringConstants.LOGGER_PUT_REQUEST_RECEIVED;
 import static edu.midlands.training.constants.StringConstants.LOGGER_REQUEST_RECEIVED;
@@ -19,6 +20,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,6 +28,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -67,4 +70,11 @@ public class CustomersController {
     return new ResponseEntity<>(customersService.updateCustomerById(customer,id), HttpStatus.OK);
   }
 
+  @DeleteMapping(value = "/{id}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void deleteCustomer(@PathVariable Long id) {
+    logger.info(new Date() + LOGGER_DELETE_REQUEST_RECEIVED + id);
+
+    customersService.deleteCustomer(id);
+  }
 }

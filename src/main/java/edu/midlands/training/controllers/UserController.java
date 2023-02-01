12087c6a14666e55395b97b4a4d157cf8d168a8d
Justin Williams <williams.jman@gmail.com>
2023-02-01 02:10:24 -0500

    Added comments to the UserController

--- src/main/java/edu/midlands/training/controllers/UserController.java
+++ src/main/java/edu/midlands/training/controllers/UserController.java
@@ -27,6 +27,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * This controller has all the CRUD methods for the user entity
+ */
 @RestController
 @RequestMapping(CONTEXT_USERS)
 public class UserController {
@@ -36,7 +39,12 @@ public class UserController {
   @Autowired
   private UserService userService;
 
-
+  /**
+   * give you all the users if you pass a null user or user matching an example with non-null user
+   *
+   * @param users user object which can have null or non-null fields, returns status 200
+   * @return List of users
+   */
   @GetMapping
   public ResponseEntity<List<User>> queryUsers(User users) {
     logger.info(new Date() + LOGGER_REQUEST_RECEIVED + users.toString());
@@ -44,6 +52,12 @@ public class UserController {
     return new ResponseEntity<>(userService.queryUsers(users), HttpStatus.OK);
   }
 
+  /**
+   * Gets user by id.
+   *
+   * @param id the user's id from the path variable
+   * @return the User with said id
+   */
   @GetMapping(value = "/{id}")
   public ResponseEntity<User> getUser(@PathVariable Long id) {
     logger.info(new Date() + LOGGER_REQUEST_RECEIVED + id);
@@ -51,6 +65,12 @@ public class UserController {
     return new ResponseEntity<>(userService.getUser(id), HttpStatus.OK);
   }
 
+  /**
+   * Adds a new user to the database.
+   *
+   * @param user the user from the request body being added
+   * @return the user if everything is correctly added
+   */
   @PostMapping
   public ResponseEntity<User> save(@Valid @RequestBody User user) {
     logger.info(new Date() + LOGGER_POST_REQUEST_RECEIVED);
@@ -58,6 +78,13 @@ public class UserController {
     return new ResponseEntity<>(userService.addUser(user), HttpStatus.CREATED);
   }
 
+  /**
+   * Update user by id
+   *
+   * @param id  the id of the user to be updated from the path variable
+   * @param user the user's new information from the request body
+   * @return the user if input and date is correct
+   */
   @PutMapping(value = "/{id}")
   public ResponseEntity<User> updateUserById(
       @PathVariable Long id, @Valid @RequestBody User user) {
@@ -66,6 +93,11 @@ public class UserController {
     return new ResponseEntity<>(userService.updateUserById(user,id), HttpStatus.OK);
   }
 
+  /**
+   * Delete user by id.
+   *
+   * @param id the user's id from the path variable
+   */
   @DeleteMapping(value = "/{id}")
   @ResponseStatus(HttpStatus.NO_CONTENT)
   public void deleteUser(@PathVariable Long id) {
